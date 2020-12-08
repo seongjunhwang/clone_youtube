a0b397547c8b27465036ca49940169b78dbaf640
seongjunhwang <samkukzi3@gmail.com>
2020-12-08 15:22:19 +0900

    route setting

--- index.js
+++ index.js
@@ -7,4 +7,17 @@ function handleListening() {
     console.log("Listening on: http://localhost:4000");
 }
 
+function handleHome(req, res) {
+    console.log(req);
+    res.send("hello from home");
+}
+
+function handleProfile(req, res) {
+    res.send("you are on my profile");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
 app.listen(PORT, handleListening);
