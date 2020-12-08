fadb274f09aefc62ee3c8b92f8698cb25d854750
seongjunhwang <samkukzi3@gmail.com>
2020-12-08 16:28:37 +0900

    morgan, helmet, cookieparser, bodyparser

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
+
 const app = express();
 
 const PORT = 4000;
@@ -10,12 +16,14 @@ const handleHome = (req, res) => res.send("hello from home haha");
 
 const handleProfile = (req, res) => res.send("you are on my profile");
 
-const betweenHome = (req, res, next) => {
-    console.log("between");
-    next();
-} 
 
-app.use(betweenHome);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(helmet());
+app.use(morgan("tiny"));
+
+
 
 app.get("/", handleHome);
 
